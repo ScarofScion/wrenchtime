8b4a1a786c3bde08d849ee821fecda75db21bfe1
scarofscion <scarofscion@gmail.com>
2019-02-19 03:59:25 -0800

    save

--- src/components/SignInDialog.js
+++ src/components/SignInDialog.js
@@ -1,22 +1,23 @@
-import React, {Component} from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import {FirebaseAuth} from 'react-firebaseui';
+import React, { Component } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { FirebaseAuth } from "react-firebaseui";
 
 class SignInDialog extends Component {
   constructor(props) {
     super(props);
     this.firebaseUiConfig = {
-      signInFlow: 'popup',
+      signInFlow: "popup",
       signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
       ],
       callbacks: {
         signInSuccessWithAuthResult: result => {
           this.props.authService.signIn(result.user);
-        },
-      },
+        }
+      }
     };
   }
 
